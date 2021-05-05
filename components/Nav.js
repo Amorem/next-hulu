977c370f1b0ac55022658d7de8c69d2135f751c7
Cedric <cedric@pop-os.localdomain>
2021-05-05 19:04:36 +0200

    scrollbar-hide

--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,20 @@
+import requests from "../utils/requests";
+
 function Nav() {
-  return <nav></nav>;
+  return (
+    <nav>
+      <div className="flex px-10 space-x-10 overflow-x-scroll text-2xl sm:px-20 whitespace-nowrap sm:space-x-20 scrollbar-hide">
+        {Object.entries(requests).map(([key, { title, url }]) => (
+          <h2
+            key={key}
+            className="transition duration-100 transform cursor-pointer last:pr-24 hover:scale-125 hover:text-white active:text-red-500"
+          >
+            {title}
+          </h2>
+        ))}
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
